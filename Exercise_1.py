cbe6643762b22e154bec8467eb258cdbf3a44195
PrajwalS Venkateshmurthy <psv.globallyunique@gmail.com>
2025-01-18 18:24:24 -0800

    Exercise_1 : Binary Search.

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,48 @@
-# Python code to implement iterative Binary  
-# Search. 
-  
-# It returns location of x in given array arr  
-# if present, else returns -1 
-def binarySearch(arr, l, r, x): 
-  
-  #write your code here
-  
+""" 
+Binary Search Implementation (Iterative Solution)
+
+# Time Complexity :
+- O(log n) where n is the size of array (each iteration reduces search space by half)
+  - Base 2 log due to binary splitting
+
+# Space Complexity :
+- O(1) (only using constant extra space for variables l, r, m)
+
+# Did this code successfully run on Leetcode :
+- Yes
+
+# Any problem you faced while coding this :
+- No
+"""
+
+def binarySearch(arr, l, r, x):
+    while l <= r:  
+        m = (l + r) // 2
+        
+        # If element found at mid, return index
+        if arr[m] == x:
+            return m
+        
+        # If element is larger than mid, ignore left half else ignore right half
+        if arr[m] > x:
+            r = m - 1
+        else:
+            l = m + 1
+
+    return -1
     
-  
 # Test array 
-arr = [ 2, 3, 4, 10, 40 ] 
-x = 10
+arr = [2, 3, 4, 10, 40]  # Must be sorted for binary search
   
-# Function call 
-result = binarySearch(arr, 0, len(arr)-1, x) 
-  
-if result != -1: 
-    print "Element is present at index % d" % result 
-else: 
-    print "Element is not present in array"
+# Test cases for elements in array
+assert binarySearch(arr, 0, len(arr)-1, 2) == 0
+assert binarySearch(arr, 0, len(arr)-1, 3) == 1
+assert binarySearch(arr, 0, len(arr)-1, 4) == 2
+assert binarySearch(arr, 0, len(arr)-1, 10) == 3
+assert binarySearch(arr, 0, len(arr)-1, 40) == 4
+
+# Test cases for elements not in array
+assert binarySearch(arr, 0, len(arr)-1, 1) == -1
+assert binarySearch(arr, 0, len(arr)-1, 50) == -1
+
+print("All tests passed")
